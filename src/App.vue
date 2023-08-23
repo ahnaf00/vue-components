d5a30c96a39b8656eedcc1cb3f48537f4394c3af
Ahnaf Anan <aanana0171@gmail.com>
2023-08-24 02:39:57 +0600

    Plans

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,60 @@
-<script setup>``
-import BlogPost from './components/BlogPost.vue';
+<script setup>
+import MyPlans from './components/MyPlans.vue';
+const packages = [
+  {
+    title:"Starter",
+    about:"Best option for personal use & for your next project.",
+    price:29,
+    uses:[
+      "Individual configuration",
+      "No setup, or hidden fees",
+      "Team size: 1 developer",
+      "Premium support: 12 months",   
+      "Free updates: 12 months"   
+    ]
 
-const posts=[
-  {
-    title: "Deadbeef Decrementation",
-    excerpt: "How to decrement from a magic debug value to zero.",
-    tags: "spirituality",
-    date: "2016-10-09",
-    linkColor:"#6366F1"
   },
   {
-    title: "Interior Crocodile Alligator, I Drive a Chevrolet Movie Theater",
-    excerpt: "♫ Interior Crocodile Alligator, I Drive a Chevrolet Movie Theater ♫",
-    tags: "cats",
-    date: "2008-09-19",
-    linkColor:"#F59E0B"
+    title:"Company",
+    about:"Relevant for multiple users, extended & premium support.",
+    price:99,
+    uses:[
+      "Individual configuration",
+      "No setup, or hidden fees",
+      "Team size: 10 developers",
+      "Premium support: 24 months",
+      "Free updates: 24 months"
+    ]
   },
   {
-    title: "Why My Practice of Thelema Makes Me Better at World of Warcraft",
-    excerpt: "\"Do what thou wilt\" shall be the whole of the Law.",
-    tags: "enchiladas",
-    date: "1974-09-09",
-    linkColor:"#10B981"
-  },
-  {
-    title:'Shrek Predicted the COVID-19 Pandemic',
-    excerpt:'Find out how Shrek is disrupting astrology.',
-    tags:"orcas",
-    date:'1921-04-16',
-    linkColor:"#EF4444",
-  },
-  {
-    title:'Lorem Ipsum',
-    excerpt:'You know what time it is: lorem ipsum time.',
-    tags:'stoicism',
-    date:'1918-11-04',
-    linkColor:"#8B5CF6"
+    title:"Enterprise",
+    about:"Best for large scale uses and extended redistribution rights.",
+    price:499,
+    uses:[
+      "Individual configuration",
+      "No setup, or hidden fees",
+      "Team size: 100+ developers",
+      "Premium support: 36 months",
+      "Free updates: 36 months"
+    ]
   }
 ]
 </script>
 
 <template>
   <section class="mx-auto ">
-    <header>
-      <nav class="container mx-auto max-w-3xl px-8 pt-2 flex flex-wrap justify-between">
-        <div>
-          <h1>
-            <a href="/11r/">11r</a>
-          </h1>
-          <p>america's favorite eleventy blog template</p>
+    <section class="bg-white dark:bg-gray-900">
+      <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed for business teams like yours</h2>
+          <p class="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
         </div>
-
-        <ul class="flex flex-wrap sm:w-32 w-full mt-6 md:justify-between justify-evenly">
-          <li>
-            <a href="/11r/404.html">404</a>
-          </li>
-
-          <li>
-            <a href="/11r/about">About</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-    <main class="container mx-auto max-w-3xl p-8 grow">
-
-      <div>
-
-
-        <BlogPost :BlogPosts="posts" />
-
-        <p class="text-center text-sm italic mb-10">(On page
-          1
-          of
-          2.)</p>
+        <div class="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
+          <!-- Pricing Card -->
+          <MyPlans :plans="packages"/>
+        </div>
       </div>
-
-      <div class="grid grid-flow-row grid-cols-3 gap-4 mb-2">
-        <a class="tag cats mx-auto" href="/11r/tag/cats">cats</a>
-        <a class="tag enchiladas mx-auto" href="/11r/tag/enchiladas">enchiladas</a>
-        <a class="tag beer mx-auto" href="/11r/tag/beer">beer</a>
-        <a class="tag spirituality mx-auto" href="/11r/tag/spirituality">spirituality</a>
-        <a class="tag orcas mx-auto" href="/11r/tag/orcas">orcas</a>
-        <a class="tag stoicism mx-auto" href="/11r/tag/stoicism">stoicism</a>
-      </div>
-
-
-    </main>
+    </section>
   </section>
 </template>
 
