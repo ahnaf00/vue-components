48276d31ce6baba7dc6bd0b7fd027e2de6911034
Ahnaf Anan <aanana0171@gmail.com>
2023-08-24 01:43:07 +0600

    Blog Post

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,94 @@
-<script setup>
-import CardBody from "./components/CardBody.vue";
+<script setup>``
+import BlogPost from './components/BlogPost.vue';
 
-const card_1 = {
-  title:"The Magnificent Bogra",
-  para:"Located in Rajshahi Division, Bogra is one of the oldest and most fascinating towns in Bangladesh",
-  img:"https://i.ibb.co/Kr4b0zJ/152013403-10158311889099633-8423107287930246533-o.jpg",
-  distanceInfo:"196 km",
-  reviews:"16 reviews",
-  placesToVisit:"*Places to visit: Mahasthangarh, Vasu Bihar & Momo Inn",
-  btnLink:"Book Ticket"
-}
-
-
+const posts=[
+  {
+    title: "Deadbeef Decrementation",
+    excerpt: "How to decrement from a magic debug value to zero.",
+    tags: "spirituality",
+    date: "2016-10-09",
+    linkColor:"#6366F1"
+  },
+  {
+    title: "Interior Crocodile Alligator, I Drive a Chevrolet Movie Theater",
+    excerpt: "♫ Interior Crocodile Alligator, I Drive a Chevrolet Movie Theater ♫",
+    tags: "cats",
+    date: "2008-09-19",
+    linkColor:"#F59E0B"
+  },
+  {
+    title: "Why My Practice of Thelema Makes Me Better at World of Warcraft",
+    excerpt: "\"Do what thou wilt\" shall be the whole of the Law.",
+    tags: "enchiladas",
+    date: "1974-09-09",
+    linkColor:"#10B981"
+  },
+  {
+    title:'Shrek Predicted the COVID-19 Pandemic',
+    excerpt:'Find out how Shrek is disrupting astrology.',
+    tags:"orcas",
+    date:'1921-04-16',
+    linkColor:"#EF4444",
+  },
+  {
+    title:'Lorem Ipsum',
+    excerpt:'You know what time it is: lorem ipsum time.',
+    tags:'stoicism',
+    date:'1918-11-04',
+    linkColor:"#8B5CF6"
+  }
+]
 </script>
 
 <template>
   <section class="mx-auto ">
-    <h1 class="my-5 text-center">Components</h1>
-    <div class="flex flex-col space-y-5  text-left">
+    <header>
+      <nav class="container mx-auto max-w-3xl px-8 pt-2 flex flex-wrap justify-between">
+        <div>
+          <h1>
+            <a href="/11r/">11r</a>
+          </h1>
+          <p>america's favorite eleventy blog template</p>
+        </div>
 
-      <CardBody :cards="card_1"/>
+        <ul class="flex flex-wrap sm:w-32 w-full mt-6 md:justify-between justify-evenly">
+          <li>
+            <a href="/11r/404.html">404</a>
+          </li>
 
-      <CardBody :cards="card_1"/>
-    </div>
+          <li>
+            <a href="/11r/about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main class="container mx-auto max-w-3xl p-8 grow">
 
+      <div>
+
+
+        <BlogPost :BlogPosts="posts" />
+
+        <p class="text-center text-sm italic mb-10">(On page
+          1
+          of
+          2.)</p>
+      </div>
+
+      <div class="grid grid-flow-row grid-cols-3 gap-4 mb-2">
+        <a class="tag cats mx-auto" href="/11r/tag/cats">cats</a>
+        <a class="tag enchiladas mx-auto" href="/11r/tag/enchiladas">enchiladas</a>
+        <a class="tag beer mx-auto" href="/11r/tag/beer">beer</a>
+        <a class="tag spirituality mx-auto" href="/11r/tag/spirituality">spirituality</a>
+        <a class="tag orcas mx-auto" href="/11r/tag/orcas">orcas</a>
+        <a class="tag stoicism mx-auto" href="/11r/tag/stoicism">stoicism</a>
+      </div>
+
+
+    </main>
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { reactive } from "vue";
+defineProps(["BlogPosts"]);
+const customStyle = reactive({
+    backgroundColor: "red",
+})
+</script>
+
+<template>
+    <div class="mb-8 border-dotted border-b pb-8 border-gray-300" v-for="(post, index) in BlogPosts" :key="index">
+        <h2>
+        <a class="block" href="/11r/post/deadbeef-decrementation/">{{ post.title }}</a>
+        </h2>
+
+        <p class="excerpt">{{ post.excerpt }}</p>
+
+        <a class="tag spirituality"  :style="{ backgroundColor:post.linkColor }" href="/11r/tag/spirituality">{{ post.tags }}</a>
+
+        <span class="block md:inline md:float-right md:pt-4 pt-2 created-date" datetime="Sat Oct 08 2016 19:00:00 GMT-0500 (Central Daylight Time)">
+        {{ post.date }}
+        </span>
+    </div>
+</template>
+
+<style scoped></style>
